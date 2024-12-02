1895a52328f7d3439e4dcb4414aada9ab9f08015
Neal <neal17889@gmail.com>
2024-12-03 01:09:41 +0800

    Delete Assets directory

--- Assets/Scripts/Entities/Character.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Character : Singleton<Character>
-{
-    public Queue<Vector3> PositionInfo = new Queue<Vector3>();
-}
